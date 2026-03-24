7e1af73faf85e7cea7ac82f8331597ebb5d6460b
pbatsaihan <batsaihan.p@gmail.com>
2026-03-24 21:36:20 +0800

    dev: configure local frontend for shared dev backend

--- web-dev-server.config.mjs
+++ web-dev-server.config.mjs
@@ -1,6 +1,7 @@
 import {fromRollup} from '@web/dev-server-rollup'
 import rollupReplace from '@rollup/plugin-replace'
 import {esbuildPlugin} from '@web/dev-server-esbuild'
+import proxy from 'koa-proxies'
 
 const replace = fromRollup(rollupReplace)
 
@@ -9,13 +10,27 @@ export default {
     esbuildPlugin({ts: true}),
     replace({
       include: [
+        'src/**/*.js',
+        'src/**/*.ts',
         'node_modules/@popperjs/**/*.js',
         'node_modules/@popperjs/**/*.ts',
         'node_modules/tippy.js/**/*.ts',
-        'node_modules/tippy.js/**/*.js'
+        'node_modules/tippy.js/**/*.js',
       ],
       preventAssignment: true,
-      'process.env.NODE_ENV': '"production"'
-    })
-  ]
+      'process.env.NODE_ENV': '"production"',
+      'http://localhost:5555': '',
+    }),
+  ],
+  middleware: [
+    proxy('/api', {
+      target: 'http://131.186.36.129:5001',
+      changeOrigin: true,
+    }),
+    proxy('/socket.io', {
+      target: 'http://131.186.36.129:5001',
+      changeOrigin: true,
+      ws: true,
+    }),
+  ],
 }
